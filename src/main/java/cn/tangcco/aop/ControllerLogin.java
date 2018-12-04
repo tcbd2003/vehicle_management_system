cda3008d8ee56b27e167323e3400946cc5f698da
hp <13520216547@189.cn>
2018-12-04 17:19:58 +0800

    IDEA整合GitHub测试123457777777

--- src/main/java/cn/tangcco/aop/ControllerLogin.java
+++ src/main/java/cn/tangcco/aop/ControllerLogin.java
@@ -12,7 +12,7 @@ public class ControllerLogin {
     private final Logger logger=Logger.getLogger("ControllerLogin");
 
     public void before(JoinPoint joinPoint){
-        logger.info("调用了:"+joinPoint.getTarget()+"类的,"+joinPoint.getSignature().getName()+"方法,入参:"+ Arrays.toString(joinPoint.getArgs()));
+       // logger.info("调用了:"+joinPoint.getTarget()+"类的,"+joinPoint.getSignature().getName()+"方法,入参:"+ Arrays.toString(joinPoint.getArgs()));
     }
     public void after(JoinPoint joinPoint,Object result){
         logger.info("调用了:"+joinPoint.getTarget()+"类的,"+joinPoint.getSignature().getName()+"方法,返回"+result);
