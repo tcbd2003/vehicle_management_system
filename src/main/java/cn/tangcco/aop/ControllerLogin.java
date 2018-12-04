40afde018a86b3400832b672acd11e60c7aeb96c
hp <13520216547@189.cn>
2018-12-04 21:37:07 +0800

    全部

--- src/main/java/cn/tangcco/aop/ControllerLogin.java
+++ src/main/java/cn/tangcco/aop/ControllerLogin.java
@@ -7,17 +7,12 @@ import java.util.logging.Logger;
 
 public class ControllerLogin {
 
-
-
     private final Logger logger=Logger.getLogger("ControllerLogin");
 
     public void before(JoinPoint joinPoint){
-        //dsfsdf
-        //ssdffs
-        //sdfasf
-       // logger.info("调用了:"+joinPoint.getTarget()+"类的,"+joinPoint.getSignature().getName()+"方法,入参:"+ Arrays.toString(joinPoint.getArgs()));
+       logger.info("调用了:"+joinPoint.getTarget()+"类的,"+joinPoint.getSignature().getName()+"方法,入参:"+ Arrays.toString(joinPoint.getArgs()));
     }
     public void after(JoinPoint joinPoint,Object result){
-      //  logger.info("调用了:"+joinPoint.getTarget()+"类的,"+joinPoint.getSignature().getName()+"方法,返回"+result);
+        logger.info("调用了:"+joinPoint.getTarget()+"类的,"+joinPoint.getSignature().getName()+"方法,返回"+result);
     }
 }
